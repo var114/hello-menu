be3054ec14c6a6611252ab1daaa2e61a16d71cc5
Patil Varvarian <Patil.Varvarian@gmail.com>
2014-03-08 00:17:48 -0800

    feowjf

---
+++ hello.js
@@ -0,0 +1,64 @@
+
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path'); 
+var mime = require('mime');
+var cache = {}
+
+
+
+
+  var express = require('express');
+  var app = express();
+
+
+/*
+
+app.get('/upload', function (request, response) {
+  console.log('received GET request');
+  response.send('<form method="POST"><input type="submit"></form>');
+});
+app.post('/upload', function (request, response){
+  console.log('received POST request');
+  response.send('hey we got your request!');
+});
+
+*/
+
+app.use(express.bodyParser());
+
+app.get('/', function (req, res) {
+  var filepath = (__dirname + '/public/index.html');
+    console.log('received GET request');
+    res.sendfile(filepath);
+    /* console.log(req) */
+}); 
+
+  app.post('/', function (req, res) {
+    console.log('received POST request');
+     /* console.log(req.files.menu); */
+         res.send('file uploaded');
+
+         /*
+    fs.readFile(req.files.menu.path, function (err, data) {
+      var newPath = __dirname + "/patil/patilMenu";
+      fs.writeFile(newPath, data, function (err) {
+        res.redirect("back");
+      });
+          
+   });
+    */
+  
+});
+
+  app.listen(3000, function() { /* starts the server, port 3000. Can eventually change server to your own!!! */
+    console.log("Server listening on port 3000.");
+
+  });
+
+
+
+
+
+ 
